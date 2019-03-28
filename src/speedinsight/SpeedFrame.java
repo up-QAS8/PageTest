a438bada78fce1a1d57b4810d3c026f236208ccd
up-QAS8 <up-QAS8@protonmail.com>
2019-03-29 02:01:41 +1300

    added functionality

--- src/speedinsight/SpeedFrame.java
+++ src/speedinsight/SpeedFrame.java
@@ -4,18 +4,16 @@
  * and open the template in the editor.
  */
 package speedinsight;
- 
+
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By; 
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -24,12 +22,14 @@ import org.openqa.selenium.support.ui.WebDriverWait;
  * @author Arib
  */
 public class SpeedFrame extends javax.swing.JFrame {
-
+    private WebDriver driver; 
     /**
      * Creates new form SpeedFrame
      */
     public SpeedFrame() {
+        
         initComponents();
+        jTable1.getColumnModel().getColumn(1).setWidth(50);
     }
 
     /**
@@ -45,11 +45,14 @@ public class SpeedFrame extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
         jLabel1 = new javax.swing.JLabel();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        jTextArea2 = new javax.swing.JTextArea();
         jLabel2 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jProgressBar1 = new javax.swing.JProgressBar();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
 
         jButton1.setText("Get Speed");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -64,17 +67,41 @@ public class SpeedFrame extends javax.swing.JFrame {
 
         jLabel1.setBackground(new java.awt.Color(204, 0, 0));
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        jLabel1.setText("Input URL Here:");
+        jLabel1.setText("INPUT URL HERE");
 
-        jTextArea2.setEditable(false);
-        jTextArea2.setColumns(20);
-        jTextArea2.setRows(5);
-        jTextArea2.setAutoscrolls(false);
-        jScrollPane2.setViewportView(jTextArea2);
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel2.setText("RESULTS");
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(255, 0, 0));
-        jLabel2.setText("Results: Mobile & Desktop");
+        jButton2.setText("Stop");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "URL", "Mobile", "Desktop"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                true, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jTable1.setCellSelectionEnabled(true);
+        jTable1.setColumnSelectionAllowed(true);
+        jTable1.getTableHeader().setReorderingAllowed(false);
+        jScrollPane3.setViewportView(jTable1);
+
+        jProgressBar1.setForeground(new java.awt.Color(255, 0, 0));
+        jProgressBar1.setFocusCycleRoot(true);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -83,91 +110,130 @@ public class SpeedFrame extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(156, 156, 156)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 181, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
-                                .addGap(22, 22, 22)
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 437, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18))
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addGap(125, 125, 125)
+                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 181, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 162, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
                                 .addContainerGap()
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 619, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane3, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
                                 .addComponent(jLabel1)
-                                .addGap(159, 159, 159)))
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jLabel2)
-                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(43, Short.MAX_VALUE))
+                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 34, Short.MAX_VALUE)
-                    .addComponent(jLabel2))
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 30, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 312, Short.MAX_VALUE)
-                    .addComponent(jScrollPane1))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>                        
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
-        WebDriver driver  = new ChromeDriver(); 
-        driver.navigate().to("https://developers.google.com/speed/pagespeed/insights/");
-        
-          String data[] = jTextArea1.getText().split("\\r?\\n");
-          List mobile = new ArrayList();
-          List desktop = new ArrayList();
-          
-          for(int i = 0; i < data.length; i++ ){
-             System.out.println(data[i]);
-            driver.findElement(By.name("url")).clear();
-            driver.findElement(By.name("url")).sendKeys(data[i]);
-            driver.findElement(By.xpath("//*[contains(text(), 'ANALYZE')]")).click();
-            
-            WebDriverWait wait = new WebDriverWait(driver, 20); 
-            
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class='goog-control result']")));
-            mobile.add(driver.findElement(By.xpath("//*[@class='lh-gauge__percentage']")).getText());
+    private WebDriver initDriver(){
+        System.setProperty("webdriver.chrome.driver", "Chromedriver\\chromedriver.exe");
+        return new ChromeDriver();
+    }
 
-            try {
-                TimeUnit.MILLISECONDS.sleep(2000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(SpeedFrame.class.getName()).log(Level.SEVERE, null, ex);
-            }            
-            
-            driver.findElement(By.xpath("//*[contains(text(), 'Desktop')]")).click();
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='goog-control result'][contains(@style,'position: relative; z-index: 1; opacity: 1;')]")));
-            
-            try {
-                TimeUnit.MILLISECONDS.sleep(2000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(SpeedFrame.class.getName()).log(Level.SEVERE, null, ex);
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+
+        Thread t;
+        t = new Thread(){
+            @Override
+            public void run(){
+                driver = initDriver();
+
+                driver.navigate().to("https://developers.google.com/speed/pagespeed/insights/");
+                String data[] = jTextArea1.getText().split("\\r?\\n");
+                List mobile = new ArrayList();
+                List desktop = new ArrayList();
+
+                int progress = 0;
+                jProgressBar1.setValue(progress);
+                for(int i = 0; i < data.length; i++ ){
+                    progress += (99/data.length);
+                    
+                    driver.findElement(By.name("url")).clear();
+                    driver.findElement(By.name("url")).sendKeys(data[i]);
+                    driver.findElement(By.xpath("//*[contains(text(), 'ANALYZE')]")).click();
+
+                    WebDriverWait wait = new WebDriverWait(driver, 30);
+
+                    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class='goog-control result']")));
+                    mobile.add(driver.findElement(By.xpath("//*[@class='lh-gauge__percentage']")).getText());
+
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(2000);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(SpeedFrame.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    
+                    jProgressBar1.setValue(progress);
+
+                    driver.findElement(By.xpath("//*[contains(text(), 'Desktop')]")).click();
+                    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='goog-control result'][contains(@style,'position: relative; z-index: 1; opacity: 1;')]")));
+
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(2000);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(SpeedFrame.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    desktop.add(driver.findElement(By.xpath("//*[@class='result-container']")).getText().split("\n")[0]);
+                    System.out.println(mobile.get(i) + "        " + desktop.get(i) +"\n");
+                    
+                    Object[] row = { data[i], mobile.get(i), desktop.get(i) }; 
+                    DefaultTableModel model = (DefaultTableModel) jTable1.getModel(); 
+                    model.addRow(row); 
+                    
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(30000);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(SpeedFrame.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    
+                    
+                }
+                   
+                jProgressBar1.setValue(100);
+                driver.quit();
             }    
-            
-            desktop.add(driver.findElement(By.xpath("//*[@class='result-container']")).getText().split("\n")[0]);
-                
-            
-            System.out.println(mobile.get(i) + "        " + desktop.get(i) +"\n");
-            jTextArea2.append(mobile.get(i) + "        " + desktop.get(i) +"\n");
-          
-            try {
-                TimeUnit.MILLISECONDS.sleep(1000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(SpeedFrame.class.getName()).log(Level.SEVERE, null, ex);
-            }
-          } 
-           
-            driver.quit();
+        };
+
+        t.start();
+    }                                        
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+//        currentThread().getThreadGroup().destroy();
+        driver.quit();
     }                                        
 
     /**
@@ -176,22 +242,20 @@ public class SpeedFrame extends javax.swing.JFrame {
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                
-                
-                new SpeedFrame().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new SpeedFrame().setVisible(true);
         });
     }
 
     // Variables declaration - do not modify                     
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JProgressBar jProgressBar1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTable jTable1;
     private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextArea jTextArea2;
     // End of variables declaration                   
 }
